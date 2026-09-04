cd53f6fa8baee1378704e639d817d5b2f4905f27
Herbert-LuDwIG <robauteguilliman852@gmail.com>
2026-09-04 09:18:36 +0300

    Programm v1

---
+++ src/GitTest/Kaulins.java
@@ -0,0 +1,14 @@
+package GitTest;
+
+import java.util.Random;
+
+public class Kaulins {
+
+	public static void main(String[] args) {
+		int sk;
+		Random rand = new Random();
+		sk = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: " + sk);
+	}
+
+}
