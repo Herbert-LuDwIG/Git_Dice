649755717c47ea555ba88a8b75f51dfdb36356a6
Herbert-LuDwIG <robauteguilliman852@gmail.com>
2026-09-07 10:01:20 +0300

    Pievienots cikls

--- src/GitTest/Kaulins.java
+++ src/GitTest/Kaulins.java
@@ -4,18 +4,19 @@ import java.util.Random;
 
 public class Kaulins {
    
-	static void mestKaulinu() {// mestKaulinu()
+	static void mestKaulinu(int reizes) {// mestKaulinu()
 			int sk;
 		Random rand = new Random();
+		for(int i = 1; i<=reizes; i++) {
 		sk = rand.nextInt(6)+1;
-		
 		System.out.println("Uzkrita skaitlis: " + sk);
+	}
     }//mestKaulinu() END
 	
 	
 	
 	
 	public static void main(String[] args) {
-		mestKaulinu();	
+		mestKaulinu(5);	
 	}
 }
