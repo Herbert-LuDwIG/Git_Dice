910de8415512c5376c07ca25247c6a81619e93b7
Herbert-LuDwIG <robauteguilliman852@gmail.com>
2026-09-07 09:57:05 +0300

    Ieviesta metode2

--- src/GitTest/Kaulins.java
+++ src/GitTest/Kaulins.java
@@ -3,12 +3,19 @@ package GitTest;
 import java.util.Random;
 
 public class Kaulins {
-
-	public static void main(String[] args) {
-		int sk;
+   
+	static void mestKaulinu() {// mestKaulinu()
+			int sk;
 		Random rand = new Random();
 		sk = rand.nextInt(6)+1;
+		
 		System.out.println("Uzkrita skaitlis: " + sk);
+    }//mestKaulinu() END
+	
+	
+	
+	
+	public static void main(String[] args) {
+		mestKaulinu();	
 	}
-
 }
