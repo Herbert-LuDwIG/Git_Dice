491e3717262252fcd0ce38597d8bd8ed921002ba
Herbert-LuDwIG <robauteguilliman852@gmail.com>
2026-09-07 10:07:38 +0300

    Ieviesta iespeja reizu skaitu noradit no konsoles

--- src/GitTest/Kaulins.java
+++ src/GitTest/Kaulins.java
@@ -1,14 +1,22 @@
 package GitTest;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
 
 	public static void main(String[] args) {
-		int sk;
+		int sk, reizes;
 		Random rand = new Random();
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes metisim kaulinu?");
+		reizes = scan.nextInt();
+		
+		for(int i = 1; i <= reizes; i++) {
 		sk = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: " + sk);
+		}
+		scan.close();
 	}
-
 }
