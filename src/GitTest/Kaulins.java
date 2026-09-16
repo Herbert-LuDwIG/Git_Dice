ada9cb434d90cf42c077c12fa25578b132f06edf
Herbert <robauteguilliman852@gmail.com>
2026-09-16 14:29:58 +0300

    Now code counts smth

--- src/GitTest/Kaulins.java
+++ src/GitTest/Kaulins.java
@@ -13,19 +13,20 @@ public class Kaulins {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Cik reizes metisim kaulinu?");
 		reizes = scan.nextInt();
-		mestKaulinu(reizes);
+		System.out.println("Visu ieguto skaitļu summa: " + mestKaulinu(reizes));
 		scan.close();
 	}
 
    
-	static void mestKaulinu(int reizes) {// mestKaulinu()
-			int sk;
+	static int mestKaulinu(int reizes) {// mestKaulinu()
+			int sk, sum = 0;
 		Random rand = new Random();
 		for(int i = 1; i<=reizes; i++) {
 		sk = rand.nextInt(6)+1;
+		sum+=sk;
 		System.out.println("Uzkrita skaitlis: " + sk);
 		}
-		
+		return sum;
 	}//mestKaulinu() END
 	
     }
